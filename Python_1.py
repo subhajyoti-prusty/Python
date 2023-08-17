1c359ede697196196632676f3a17e79e395de757
Subhajyoti Prusty <70449522+subhajyoti-prusty@users.noreply.github.com>
2023-08-17 13:44:10 +0530

    Add files via upload

---
+++ Python_1.py
@@ -0,0 +1,26 @@
+# Variables-> Which stores the value 
+'''_num1_,num,Num,Num1,'''
+# String-> set is a set of characters written in a single or double quotation
+name='Subha'
+print(name)
+
+#Int-> a number without a decimal value 
+num1=10
+num2=20
+print(num1+num2)
+print(type(num1))
+
+#Float-> a number with decimal value
+num3=34.45
+num4=63.89
+sub= num3-num4
+print(sub)
+print(type(sub))
+
+num5=30
+num6=38.6
+add=num5+num6
+# Concatenation-> combining the data
+print('The additin of num5 and num6 is :',add)
+print('The values are',add,"\n total")
+print(type(add))
