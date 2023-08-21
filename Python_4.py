128f5bf872242412941fd79e4552b1f48e0d296c
Subhajyoti Prusty <70449522+subhajyoti-prusty@users.noreply.github.com>
2023-08-22 00:01:49 +0530

    Add files via upload

---
+++ Python_4.py
@@ -0,0 +1,32 @@
+# Converting Sets from immutable to mutable 
+
+a=frozenset(["E","F"])
+print(a)
+
+# Functions -> A set of data will be executed whenever we call it 
+print("Hello and good evening")
+print("Thank You")
+
+def a():    # It is the indentation of a function    def a(): 
+  print("Hello and good evening everyone ")
+  print("Thank You all")
+
+a()
+a()
+a()
+a()
+
+def name():
+  a="Subha"
+  print("My favorite student is",a)
+# If you call the function inside the function then it will be a infinite loop
+name()
+name()
+name()
+name()
+name()
+
+def add(num1,num2):                # Here num1 and num2 are parameters....... and parameters are nothing but is like variables and wu can store values in it 
+  print("The addition on the two values are",(num1+num2))
+add(12,23)         
+add(10,20)
