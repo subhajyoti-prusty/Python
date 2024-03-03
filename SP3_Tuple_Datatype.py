e81e667cdcc656e4cf62120ab3b8ccf324545e10
Subhajyoti Prusty <70449522+subhajyoti-prusty@users.noreply.github.com>
2024-03-03 10:03:18 +0530

    Add files via upload

---
+++ SP3_Tuple_Datatype.py
@@ -0,0 +1,18 @@
+# Tuple-> ordered collection of datatype,(),immutable
+mytuple=("Data",1,33.4,'Task')
+print(mytuple)
+
+print("The tuple is : ",len(mytuple))
+
+print(type(mytuple))
+
+del mytuple
+print("The tuple is deleted")
+
+# Accessing the elements of tuple
+mytuple=("Data",1,33.4,'Task')
+print(mytuple[0])
+print(mytuple[1])
+print(mytuple[2])
+print(mytuple[3])
+
